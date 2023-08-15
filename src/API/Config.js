594ed720bee7e6b9c194f62d71e206fafd26193a
hieudhd942 <ghoul.handle@gmail.com>
2023-08-16 00:24:20 +0700

    chane config

--- src/API/Config.js
+++ src/API/Config.js
@@ -1,5 +1,5 @@
 const config = {
-    HOST: "http://34.87.43.29:8080",
-    WS: "http://34.87.43.29:8080/ws"
+    HOST: "http://34.87.43.29:8000",
+    WS: "http://34.87.43.29:8000/ws"
 };
 export default config;
